907c6c97e1a9fafa396864640b75d5c7d8c09871
James <jsmilton05@gmail.com>
2014-06-22 19:20:07 +0100

    Testing NSURLRequest with Swift

---
+++ SwiftWeather/ApiClient.swift
@@ -0,0 +1,10 @@
+//
+//  ApiClient.swift
+//  SwiftWeather
+//
+//  Created by James Milton on 22/06/2014.
+//  Copyright (c) 2014 James Milton. All rights reserved.
+//
+
+import Foundation
+
--- SwiftWeather/ViewController.swift
+++ SwiftWeather/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSURLConnectionDataDelegate {
+    
+    var receivedData: NSMutableData = NSMutableData(capacity: 0)
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        test({
+            (s:String) in
+            println(s)
+            println("sdgjhcvd")
+        })
+        
+        var request = NSURLRequest(URL: NSURL(string: "http://james-milton.com/mvf-testfeeds/GetAccountInfo.json"), cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: 60.0)
+        
+        let connection = NSURLConnection(request: request, delegate: self, startImmediately: true)
+    }
+    
+    func test(block: ((String) -> Void)!)  {
+        block("hello")
+    }
+    
+    func connection(connection: NSURLConnection!,
+        didReceiveResponse response: NSURLResponse!) {
+            println(response)
+    }
+    
+    func connection(connection: NSURLConnection!,
+        didReceiveData data: NSData!) {
+            receivedData.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        println("hurrah, I've finished downloading!")
+        let dict = NSJSONSerialization.JSONObjectWithData(receivedData, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSDictionary
+        println(dict)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
